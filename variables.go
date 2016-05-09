04c2ea505855f7e97471a7d07b1f0336adb94b6b
Ross <codingclasses.049@gmail.com>
2016-05-09 12:53:28 -0500

    working with pointer

--- variables.go
+++ variables.go
@@ -5,13 +5,16 @@ import (
 	"reflect"
 )
 
-var  (
-	name = "Ross" // Name of subscriber
-	course = "Go Lang" // Name of current course
-	module = 3.2 // Current place in course
-)
-
 func main() {
+
+	name := "Ross"      // Name of subscriber
+	// course := "Go Lang" // Name of current course
+	module := 3.2       // Current place in course
+	ptr := &module
+
 	fmt.Println("Name is", name, "add is  of type", reflect.TypeOf(name))
 	fmt.Println("Module is", module, "add is  of type", reflect.TypeOf(module))
+	fmt.Println("Memory address of *module* variable is ", &module)
+	fmt.Println("Memory address of *module* variable is ", ptr, "add the value of *module* is", *ptr)
+
 }
