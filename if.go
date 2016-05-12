4ce19076a13cd494ca0a98664c9461acd1f0a7fc
Ross <codingclasses.049@gmail.com>
2016-05-11 19:27:08 -0500

    if nesting

--- if.go
+++ if.go
@@ -6,11 +6,21 @@ import (
 
 func main() {
 
-	if firstRank, secondRank := 39, 614; firstRank < secondRank {
-		fmt.Println("\n First course is doing better" + " then second course")
+	if firstRank, secondRank := 39,
+		614; firstRank < secondRank {
+		fmt.Println("\n First course is doing better" +
+			 	" then second course")
+		if firstRank > 100 {
+			fmt.Println("Error 1")
+		}
 	} else if firstRank > secondRank {
-		fmt.Println("\nOh dear... your first" + "course must be doing abysmally!")
+		fmt.Println("\nOh dear... your first" +
+			 	"course must be doing abysmally!")
+			if secondRank > 100 {
+				fmt.Println("Error 2")
+			}
 	} else {
-		fmt.Println("\nBoth courses are either" + "the same or something weird is going on")
+		fmt.Println("\nBoth courses are either" +
+			 	"the same or something weird is going on")
 	}
 }
