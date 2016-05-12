91650303c2bdb9b0a8b945d4339e73b6c49ee28a
Ross <codingclasses.049@gmail.com>
2016-05-11 19:40:33 -0500

    simple switch statement looking for a string of docker

---
+++ switch.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	switch "docker" {
+	case "linux":
+		fmt.Println("\nHere are some recommanded" + " Linux courses...")
+	case "docker":
+		fmt.Println("\nHere are some recommanded" + " Docker courses...")
+	case "mac":
+		fmt.Println("\nHere are some recommanded" + " Mac courses...")
+	default:
+		fmt.Println("\nSorry, we couldn't find a match," + "why not try our Top 100 list!")
+	}
+}
