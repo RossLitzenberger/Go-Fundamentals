980d61b230493fe1a17d8df93a8504da042e37de
Ross <codingclasses.049@gmail.com>
2016-05-11 21:02:47 -0500

    breaking the fall-through

--- switch.go
+++ switch.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
-	switch "docker" {
-	case "linux":
-		fmt.Println("\nHere are some recommanded" + " Linux courses...")
-	case "docker":
-		fmt.Println("\nHere are some recommanded" + " Docker courses...")
-		fallthrough
-	case "mac":
-		fmt.Println("\nHere are some recommanded" + " Mac courses...")
-	default:
-		fmt.Println("\nSorry, we couldn't find a match," + "why not try our Top 100 list!")
+	switch  tmpNum := random(); tmpNum {
+	case 0,2,4,6,8:
+		fmt.Println("We got an even number -", tmpNum)
+	case 1,3,5,7,9:
+		fmt.Println("We got an odd number -", tmpNum)
 	}
+}
+
+func random() int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(10)
 }
