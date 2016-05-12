70ae1e7f45b9c23ad2bfe1dbabc9d586d306e855
Ross <codingclasses.049@gmail.com>
2016-05-11 19:15:48 -0500

    moved the vars into the if block

--- if.go
+++ if.go
@@ -5,11 +5,8 @@ import (
 )
 
 func main() {
-	// Variables to store course rankings
-	firstRank := "39"   //Docker Deep Dive
-	secondRank := "614" //Docker Clustering
 
-	if firstRank < secondRank {
+	if firstRank, secondRank := 39, 614; firstRank < secondRank {
 		fmt.Println("\n First course is doing better" + " then second course")
 	} else if firstRank > secondRank {
 		fmt.Println("\nOh dear... your first" + "course must be doing abysmally!")
