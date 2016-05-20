12d23f2583dc035ce3f1e6c2e840e76aa9806ac5
Ross <codingclasses.049@gmail.com>
2016-05-20 18:04:22 -0500

    appending more number to the array

---
+++ slices3.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+func main() {
+
+
+	mySlice := make([]int, 1, 4)
+	fmt.Printf("Length is: %d. \nCapcity is: %d",
+		len(mySlice), cap(mySlice))
+
+	for i := 1; i < 17; i++ {
+		mySlice = append(mySlice, i)
+		fmt.Printf("\nCapacity is: %d",cap(mySlice))
+	}
+
+
+}
