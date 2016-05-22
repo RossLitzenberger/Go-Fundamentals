795887279b9743e20e50a4707141de5c71cf5988
Ross Litz <codingclasses.049@gmail.com>
2016-05-21 19:51:43 -0500

    unorder map

---
+++ maps2.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	testMap := map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
+	
+	for key, value := range testMap {
+		fmt.Printf("\nKey is: Value is: %v", key, value)
+	}
+}
