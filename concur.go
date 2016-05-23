03e78e695506af2b7215529be88f1975dd205a2e
Ross Litz <codingclasses.049@gmail.com>
2016-05-22 19:53:11 -0500

    allowed another thread

--- concur.go
+++ concur.go
@@ -4,26 +4,28 @@ import (
 	"fmt"
 	"time"
 	"sync"
+	"runtime"
 )
 
 func main() {
 	
+	runtime.GOMAXPROCS(2)
 	var waitGrp sync.WaitGroup
 	waitGrp.Add(2)
-	
+
 	go func(){
 		defer waitGrp.Done()
-		
+
 		time.Sleep(5 * time.Second)
 		fmt.Println("Hello")
 	}()
 
 	go func()  {
-		
+
 		defer waitGrp.Done()
-		
+
 		fmt.Println("Pluralsight")
 	}()
-	
+
 	waitGrp.Wait()
 }
